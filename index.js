9064767ecc51df276584a62420c36fa9c1b7772e
naveenfsdev <naveensrpce@gmail.com>
2023-06-30 16:47:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { IncomeData } from "./router/incomeRouter.js";
+import { Expense } from "./router/ExpenseRouter.js";
+
+//defining Port
+const Port = process.env.PORT;
+
+const app = express();
+//declaring express function
+app.use(cors());
+app.use(express.json());
+//env configuration
+dotenv.config();
+
+app.get("/",(req,res)=>{
+res.status(200).json({routes:"i am working"})
+})
+
+app.use("/", IncomeData);
+app.use("/", Expense);
+
+app.listen(Port, () => console.log(`i am working${Port}`));
